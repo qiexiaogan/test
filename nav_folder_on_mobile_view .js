1941299c788940c752b61ce675d50e97981fd761
qiexiaogan <92236728+qiexiaogan@users.noreply.github.com>
2021-10-09 19:16:08 -0700

    Update nav_folder_on_mobile_view .js

--- nav_folder_on_mobile_view .js
+++ nav_folder_on_mobile_view .js
@@ -4,3 +4,10 @@
     window.location = "/office"
   })                                  
 });         
+
+  $(document).ready(function() { 
+    $(".header-menu-nav-folder--active").css("display","none!important");
+  $(".container.header-menu-nav-item").children("a[href='/office-1']").click(function(){
+    window.location = "/office"
+  })                                  
+});        
