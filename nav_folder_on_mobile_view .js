c92facfce277763b43de9c9e1b944ddc4bda86da
qiexiaogan <92236728+qiexiaogan@users.noreply.github.com>
2021-10-09 19:15:23 -0700

    Add files via upload

---
+++ nav_folder_on_mobile_view .js
@@ -0,0 +1,6 @@
+  $(document).ready(function() { 
+    $(".header-menu-nav-folder--active").css("display","none!important");
+  $(".container.header-menu-nav-item").children("a[href='/office-1']").click(function(){
+    window.location = "/office"
+  })                                  
+});         
